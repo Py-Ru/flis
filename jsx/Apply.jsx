1f69fc68c117a3fbd7ffc23fcab539a8984c9ff2
Py-Ru <ashvinmaruti@gmail.com>
2024-06-20 10:44:44 +0100

    add apply section

---
+++ jsx/Apply.jsx
@@ -0,0 +1,261 @@
+function Apply() {
+  return (
+    <section className="apply-now" id="apply">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6 align-self-center">
+            <div className="row">
+              <div className="col-lg-12 apply">
+                <div className="item">
+                  <h3>APPLY VIA WHATSAPP</h3>
+                  <p>
+                    We are excited to welcome you to our school. Join our
+                    vibrant community and start your journey towards a high
+                    school certificate. If you have any questions, feel free to
+                    reach out to us.
+                  </p>
+                  <div className="main-button-green">
+                    <div className="scroll-to-section">
+                      <a
+                        href="https://wa.me/+2348184372392?text=I'm%20interested%20in%20applying%20for%20a%20your%20certificate."
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Join Us Now!
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-6">
+            <div className="accordion" id="accordionExample">
+              <div className="accordion-item">
+                <h2 className="accordion-header" id="headingOne">
+                  <button
+                    className="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseOne"
+                    aria-expanded="false"
+                    aria-controls="collapseOne"
+                  >
+                    About FLIS
+                  </button>
+                </h2>
+                <div
+                  id="collapseOne"
+                  className="accordion-collapse collapse"
+                  aria-labelledby="headingOne"
+                  data-bs-parent="#accordionExample"
+                >
+                  <div className="accordion-body">
+                    <p>
+                      Future Leaders International School is an international
+                      school that provides a unique educational experience to
+                      its students based on project-based learning through{" "}
+                      <strong>Pre-K to K12</strong>, FLIS seeks to be a
+                      remarkable international school offering an excellent
+                      education. FLIS was founded on <em>30-07-2019</em>.
+                      <br />
+                      <br />
+                      We are located at{" "}
+                      <em>No. 15, Dakwa, off Dei-Dei junction, FCT, Abuja</em>.
+                      Our school seeks to offer a remarkable educational
+                      experience that develops its students' personal and social
+                      skills through both the school's daily life on the one
+                      hand and the extracurricular activities, which include
+                      sports, performing arts, and STEM, on the other hand.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="accordion-item">
+                <h2 className="accordion-header" id="headingTwo">
+                  <button
+                    className="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseTwo"
+                    aria-expanded="false"
+                    aria-controls="collapseTwo"
+                  >
+                    Our Belief
+                  </button>
+                </h2>
+                <div
+                  id="collapseTwo"
+                  className="accordion-collapse collapse"
+                  aria-labelledby="headingTwo"
+                  data-bs-parent="#accordionExample"
+                >
+                  <div className="accordion-body">
+                    <p>
+                      At FLIS, we believe in providing each student with a fair
+                      opportunity to learn in a positive learning environment.
+                      At FLIS, there is room for all students to stretch their
+                      minds as well as physical abilities, explore and
+                      investigate, and develop their sense of independence. We
+                      seek to create a life-long learner who is always eager and
+                      ready to learn.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="accordion-item">
+                <h2 className="accordion-header" id="headingThree">
+                  <button
+                    className="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseThree"
+                    aria-expanded="false"
+                    aria-controls="collapseThree"
+                  >
+                    School Philosophy
+                  </button>
+                </h2>
+                <div
+                  id="collapseThree"
+                  className="accordion-collapse collapse"
+                  aria-labelledby="headingThree"
+                  data-bs-parent="#accordionExample"
+                >
+                  <div className="accordion-body">
+                    <p>
+                      FLIS aims to be an unparalleled international school
+                      offering an excellent educational system. The total FLIS
+                      experience encourages students to reach their fullest
+                      intellectual, moral, creative and physical potential.
+                      <br />
+                      <br />
+                      FLIS aims at creating leaders for tomorrow as well as
+                      global citizens who can work and live anywhere in the
+                      world.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="accordion-item">
+                <h2 className="accordion-header" id="headingFour">
+                  <button
+                    className="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseFour"
+                    aria-expanded="false"
+                    aria-controls="collapseFour"
+                  >
+                    Our Mission & Our Vision
+                  </button>
+                </h2>
+                <div
+                  id="collapseFour"
+                  className="accordion-collapse collapse"
+                  aria-labelledby="headingFour"
+                  data-bs-parent="#accordionExample"
+                >
+                  <div className="accordion-body">
+                    <p>
+                      <strong>Our Mission</strong>
+                      <br />
+                      The school's mission is to be a diverse international
+                      community providing inclusive and innovative world-class
+                      education in a safe challenging learning environment to
+                      motivate students to be confident and responsible global
+                      citizens.
+                      <br />
+                      <br />
+                      <strong>Our Vision</strong>
+                      <br />
+                      We aspire to provide our students with a safe positive
+                      community where all our students are appreciated and
+                      loved. A community that encourages our students to develop
+                      to their fullest potential.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="accordion-item">
+                <h2 className="accordion-header" id="headingFive">
+                  <button
+                    className="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseFive"
+                    aria-expanded="false"
+                    aria-controls="collapseFive"
+                  >
+                    Frequently Asked Question
+                  </button>
+                </h2>
+                <div
+                  id="collapseFive"
+                  className="accordion-collapse collapse"
+                  aria-labelledby="headingFive"
+                  data-bs-parent="#accordionExample"
+                >
+                  <div className="accordion-body">
+                    <p>
+                      <strong>How do I apply?</strong>
+                      <br />
+                      FLIS has an admission process. All applications have to be
+                      processed throughout the academic year. This means parents
+                      have to finish the admission process as soon as possible
+                      to be able to find a room in our school. To start the
+                      admission process, parents can either visit the school
+                      campus or reach out to us online.
+                      <br />
+                      <br />
+                      <strong>
+                        Are there any assessment tests during the admission
+                        process?
+                      </strong>
+                      <br />
+                      FLIS has an admission process. All applications have to be
+                      processed throughout the academic year. This means parents
+                      have to finish the admission process as soon as possible
+                      to be able to find a room in our school. To start the
+                      admission process, parents can either visit the school
+                      campus or reach out to us online.
+                      <br />
+                      <br />
+                      <strong>
+                        Does FLIS accept students with special needs?
+                      </strong>
+                      <br />
+                      Yes, we accept students with special needs at FLIS
+                      <br />
+                      <br />
+                      <strong>How long is the school day?</strong>
+                      <br />
+                      The school day is 8 working hours long.
+                      <br />
+                      <br />
+                      <strong>Does the school provide transportation?</strong>
+                      <br />
+                      Yes, the school provides a first-class bus system to its
+                      students.
+                      <br />
+                      <br />
+                      <strong>Does FLIS accept mid-year transfers?</strong>
+                      <br />
+                      Yes, FLIS accepts mid-year transfers
+                      <br />
+                      <br />
+                      <strong>Does FLIS provide E-Learning options?</strong>
+                      <br />
+                      Yes, FLIS provides online learning for students outside
+                      our geographical area.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
