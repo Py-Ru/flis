5cb2be981ef924f49f67894e66ddf3c59d89a12a
Py-Ru <ashvinmaruti@gmail.com>
2024-06-20 10:28:35 +0100

    add subheader

---
+++ jsx/SubHeader.jsx
@@ -0,0 +1,44 @@
+function SubHeader() {
+  return (
+    <div class="sub-header">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-sm-8">
+            <div class="left-content">
+              <p>
+                Welcome to <em>FLIS</em> Where Tomorrow’s Leaders Begin Their
+                Journey Today.
+              </p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-sm-4">
+            <div class="right-icons">
+              <ul>
+                <li>
+                  <a href="https://web.facebook.com/people/Future-Leaders-international-schools/100076431710244/?mibextid=ZbWKwL&checkpoint_src=any">
+                    <i class="fa fa-facebook"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.instagram.com/futureleadersinternationalflis/">
+                    <i class="fa fa-instagram"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://l.instagram.com/?u=https%3A%2F%2Fwww.tiktok.com%2F%40futureleadersschflis%3F_t%3D8iVXAMKTUWr%26_r%3D1&e=AT37TsrrLLDYOcIvNTzqFk0OhKGH0YCOLrPtmwWg0afy2jsLLJlSMqQKuPVg-LOww53v-FAiAdYJUadGpPDTNUbkazvAPMHRfYSMwA">
+                    <i class="fab fa-tiktok"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://l.instagram.com/?u=https%3A%2F%2Fwww.linkedin.com%2Fcompany%2Ffuture-leaders-international-schools%2F&e=AT3zF3HOKqkqoV4Ff4Pw-TNpx-SpGvbOY7FTHgrl4TvV2Yfrcpz3ETGyn4l3zwSAVfq2382GO3saGRjwvn8GvPEtQFHXBpOX_D1gjw">
+                    <i class="fa fa-linkedin"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
